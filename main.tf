e8ed7a78af9c9e0f32486f345902a9c5bec045cf
Steven R <anguelr12@hotmail.com>
2020-11-09 15:09:23 -0500

    fmt

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Using a single workspace:
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "strevens"
 
     workspaces {
@@ -11,11 +11,11 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-east-1"
-    access_key = var.access_key
-    secret_key = var.secret_key
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 resource "aws_s3_bucket" "test" {
-    bucket_prefix = "test-"
+  bucket_prefix = "test-"
 }
